367b4f4591744f3857246412774c28548c3a55fd
Shane Charles <shanecharles@burningicesolutions.com>
2016-09-04 09:55:03 -0500

    add solution to problem 12.

---
+++ 12/problem_12.fsx
@@ -0,0 +1,14 @@
+﻿let divisors (x : int) = 
+  let sqrt = float >> System.Math.Sqrt >> int
+  seq { for i in {1 .. sqrt x} do
+          match (x % i, x / i) with 
+          | 0, y when i <> y -> yield i; yield y
+          | 0, _             -> yield i
+          | _                -> () } 
+
+let triangularNumbers = Seq.unfold (fun (i,acc) -> Some (acc + i, (i + 1, acc + i))) (1, 0)
+
+triangularNumbers
+|> Seq.filter (fun x -> x |> divisors |> Seq.length > 500) 
+|> Seq.head
+|> printfn "The value of frist triangular number with over 500 divisors: %d"
